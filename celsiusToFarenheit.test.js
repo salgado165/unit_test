e33fe03c9e714701fc1d36848f8c47ae4050d715
Carlos Daniel Salgado <salgado165@hotmail.com>
2021-06-01 03:18:46 +0000

    add yml file

--- celsiusToFarenheit.test.js
+++ celsiusToFarenheit.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@jest/globals')
 const celsiusToFarenheit = require('./celsiusToFarenheit')
 
 test('Convertin Celsius to Farenheit', ()=>{
-    expect(celsiusToFarenheit(25)).toBe('77')
+    expect(celsiusToFarenheit(30)).toBe(86);    
 });
