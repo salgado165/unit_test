5b3cfc93241b288c0cd60ab7b9fff2dd8b49f136
Carlos Daniel Salgado <salgado165@hotmail.com>
2021-06-05 04:28:27 +0000

    add img

--- ConvertTemperature.test.js
+++ ConvertTemperature.test.js
@@ -2,9 +2,9 @@ const { test, expect } = require('@jest/globals')
 const temperature = require('./ConvertTemperature')
 
 test('Convert Celsius to Fahrenheit', ()=>{    
-    expect(new temperature(12).celsiusToFahrenheit()).toBe(86);       
+    expect(new temperature(30).celsiusToFahrenheit()).toBe(86);       
 });
 
 test('Convert Fahrenheit to Celsius', ()=>{    
-    expect(new temperature(10).fahrenheitToCelsius()).toBe(0);
+    expect(new temperature(32).fahrenheitToCelsius()).toBe(0);
 })
