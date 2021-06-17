600bf6ca7cfe316d381fe343087b7312f4eeba5b
Carlos Daniel Salgado <salgado165@hotmail.com>
2021-06-17 02:44:15 +0000

    unit test pair session

--- ConvertTemperature.js
+++ ConvertTemperature.js
@@ -1,15 +1,25 @@
-class temperature{   
-    constructor(valTemperature) {
-        this.valTemperature = valTemperature;
-    }
-
-    celsiusToFahrenheit(){
-        return ((this.valTemperature * 1.8)+ 32);
-    }
-    
-    fahrenheitToCelsius(){
-        return (this.valTemperature - 32) * (0.55);
+class Calculator{   
+    sum(value){
+        if(typeof value === 'string'){
+            let sum =0;
+            let customDelim = value.substr(2,1);
+            let delimiter = new RegExp(`${customDelim}|\n|,`)
+console.log(delimiter)
+            value = value.split(delimiter);
+            
+            value.forEach(val => {   
+                if(typeof val === number){
+                    sum += parseInt(val);
+                }            
+                
+            });            
+            return sum;
+        }
+        return 0;
     }
 }
-module.exports = temperature
+
+
+
+module.exports = Calculator
 
--- ConvertTemperature.test.js
+++ ConvertTemperature.test.js
@@ -1,14 +1,38 @@
 const { test, expect } = require('@jest/globals')
-const temperature = require('./ConvertTemperature')
+const Calculator = require('./ConvertTemperature')
 
-test('Convert Celsius to Fahrenheit', ()=>{    
-    expect(new temperature(30).celsiusToFahrenheit()).toBe(86);       
+test('Given the user input is empty when calculating the sum then it should return zero.', ()=>{    
+   const calculator = new Calculator();
+   const result = calculator.sum();
+   expect(result).toBe(0);   
 });
 
-test('Convert Fahrenheit to Celsius', ()=>{    
-    expect(new temperature(32).fahrenheitToCelsius()).toBe(0);
+test('Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)',()=>{
+   const calculator = new Calculator();
+   const result = calculator.sum("3");
+   expect(result).toBe(3);
 })
 
-test('type', ()=>{
-    expect(typeof new temperature(30).fahrenheitToCelsius()).toBe('string');
+test('Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)',()=>{
+    const calculator = new Calculator();
+   const result = calculator.sum("1,2");
+   expect(result).toBe(3);
+})
+
+test('Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)',()=>{
+    const calculator = new Calculator();
+   const result = calculator.sum("1,2,3");
+   expect(result).toBe(6);
+})
+
+test('Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)', ()=>{
+    const calculator = new Calculator();
+   const result = calculator.sum("1\n2,3");
+   expect(result).toBe(6);
+})
+
+test('Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)', ()=>{
+     const calculator = new Calculator();
+   const result = calculator.sum("//;\n1;2");
+   expect(result).toBe(3);
 })
